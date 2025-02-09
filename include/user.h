9a5abdf93f508a1dd056b1d22deb05e082a1bf23
a-sad-dan <danishasad91@gmail.com>
2025-02-09 16:18:40 +0530

    function declarations updated

--- include/user.h
+++ include/user.h
@@ -16,5 +16,7 @@ struct User *find_user_by_id(int user_id);
 struct User *find_user_by_username(char *username);
 
 float find_balance_by_username(char *username);
+int find_user_id_by_username(char *username);
+int validate_receiver_id(int receiver_id);
 
 #endif // USER_H
--- include/utils.h
+++ include/utils.h
@@ -2,11 +2,12 @@
 #define UTILS_H
 #include <transaction.h>
 
-
 int generate_unique_user_id();
+int generate_unique_transaction_id();
 int validate_username(char *username);
 int *hash_password(char *password);
 void display_error(char *error_message);
 void clear_input_buffer();
+char *get_current_date();
 
 #endif
